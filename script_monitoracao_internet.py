4936a72b4f722ad491a636f90dc42319b887a527
marcosvenasc <marcosvenasc@gmail.com>
2023-05-24 23:54:09 -0300

    alteração na linha 9

--- script_monitoracao_internet.py
+++ script_monitoracao_internet.py
@@ -6,14 +6,14 @@ url = 'https://www.google.com.br'
 
 # Loop infinito para monitorar a conexão com a internet
 while True:
-try:
+ try:
 # Tentativa de conexão com a internet
-urllib.request.urlopen(url)
-print('Conectado à internet')
+  urllib.request.urlopen(url)
+  print('Conectado à internet')
 
-except urllib.error.URLError:
+ except urllib.error.URLError:
 # Exceção caso não haja conexão com a internet
-print('Sem conexão com a internet')
+  print('Sem conexão com a internet')
 
 # Tempo de espera antes da próxima verificação de conexão
 time.sleep(5)
