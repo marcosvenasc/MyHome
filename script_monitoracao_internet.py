b4693eeec5578061e4600e3e9374d194909ee63e
marcosvenasc <marcosvenasc@gmail.com>
2023-05-24 23:32:07 -0300

    Criação de script de monitoração

---
+++ script_monitoracao_internet.py
@@ -0,0 +1,19 @@
+import urllib.request
+import time
+
+# URL para teste de conexão com a internet
+url = 'https://www.google.com'
+
+# Loop infinito para monitorar a conexão com a internet
+while True:
+try:
+# Tentativa de conexão com a internet
+urllib.request.urlopen(url)
+print('Conectado à internet')
+
+except urllib.error.URLError:
+# Exceção caso não haja conexão com a internet
+print('Sem conexão com a internet')
+
+# Tempo de espera antes da próxima verificação de conexão
+time.sleep(5)
